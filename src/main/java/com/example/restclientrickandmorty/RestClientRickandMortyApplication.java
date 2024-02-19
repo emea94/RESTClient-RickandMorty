687adc6139f295c19b2315a52b2a912506eac12f
emea94 <emea94@web.de>
2024-02-19 18:17:19 +0100

    Setup Project

---
+++ src/main/java/com/example/restclientrickandmorty/RestClientRickandMortyApplication.java
@@ -0,0 +1,13 @@
+package com.example.restclientrickandmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientRickandMortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientRickandMortyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restclientrickandmorty/RestClientRickandMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restclientrickandmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientRickandMortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
